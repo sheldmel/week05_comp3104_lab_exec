05952cdf37983d68b6d55481ebccfd55f4f3dcfe
sheldmel <dmello.shelton@yahoo.com>
2020-10-06 19:43:26 -0400

    git commit -m

---
+++ build/precache-manifest.87583a30873a5754319b476c832c72b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e8931ae186fdd1c987d7080bdabd48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d647580ebaf155bb0242",
+    "url": "/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "066cd71dcc7aa23353eb",
+    "url": "/static/js/2.c64427ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c64427ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d647580ebaf155bb0242",
+    "url": "/static/js/main.13bb3f6b.chunk.js"
+  },
+  {
+    "revision": "39bad62e75b69149407c",
+    "url": "/static/js/runtime-main.340c4f66.js"
+  }
+]);
